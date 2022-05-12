c4dd537c3385b4f740f7c350e0a6228f871b436b
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-12 22:37:09 +0430

    commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,9 @@
-import { v4 as uuidv4 } from "uuid";
-
-const Todo = (text) => {
+const Todo = ({ text }) => {
+  // Events
+  const deleteHanler = () => {};
   return (
     <div className="todo">
-      <li key={uuidv4()} className="todo-item">
-        {text}
-      </li>
+      <li className="todo-item">{text}</li>
       <button className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
