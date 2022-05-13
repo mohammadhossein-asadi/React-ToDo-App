178b0c67f31915da31a4e58c55055a389ac67255
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-13 14:56:00 +0430

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 // Import components
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  // State stuff
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState("all");
-  const [filteredTodos, setFilteredTodos] = useState();
+  const [filteredTodos, setFilteredTodos] = useState([]);
+
+  // Use Effect
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  // Functions
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter((todo) => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter((todo) => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
   return (
     <div className="App">
       <header>
