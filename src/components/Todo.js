6464e55c89805ee913b5f8380eebf7db5ea6c7fd
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-13 12:48:25 +0430

    commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,15 @@
 const Todo = ({ text ,todos,setTodos }) => {
   // Events
-  const deleteHanler = () => {};
+  const deleteHanler = () => {
+setTodos(todos.filter((el) => el.id !== todo.id))
+  };
   return (
     <div className="todo">
       <li className="todo-item">{text}</li>
       <button className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
-      <button className="trash-btn">
+      <button onClick={deleteHanler} className="trash-btn">
         <i className="fas fa-trash"></i>
       </button>
     </div>
