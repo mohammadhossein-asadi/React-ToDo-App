bff9d24bf92eb5387f04bc67638a1eadcb030c88
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-05-13 13:40:58 +0430

    commit

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,7 @@
 // Import components
 import { v4 as uuidv4 } from "uuid";
 
-const Form = ({ setInputText, todos, setTodos, inputText }) => {
+const Form = ({ setInputText, todos, setTodos, inputText, setStatus }) => {
   // Here i can write javascript code and function
   const inputTextHandler = (e) => {
     console.log(e.target.value);
@@ -19,6 +19,10 @@ const Form = ({ setInputText, todos, setTodos, inputText }) => {
     ]);
     setInputText("");
   };
+
+  const statusHandler = (e) => {
+    setStatus(e.target.value);
+  };
   return (
     <form>
       <input
@@ -31,7 +35,7 @@ const Form = ({ setInputText, todos, setTodos, inputText }) => {
         <i className="fas fa-plus-square"></i>
       </button>
       <div className="select">
-        <select name="todos" className="filter-todo">
+        <select onChange={statusHandler} name="todos" className="filter-todo">
           <option value="all">All</option>
           <option value="completed">Completed</option>
           <option value="uncompleted">Uncompleted</option>
